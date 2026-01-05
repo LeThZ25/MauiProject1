1ded9c0d932ef6f95c4edc70eebd7ecdd685fca7
DuongLeThuong <dtc235340030@ictu.edu.vn>
2026-01-05 09:09:04 +0700

    Test

---
+++ MauiProject1/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace MauiProject1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			UpdateThemeIcon();
+		}
+
+		private void OnThemeSwitchTapped(object sender, TappedEventArgs e)
+		{
+			var currentTheme = Application.Current.UserAppTheme;
+
+			if (currentTheme == AppTheme.Unspecified)
+				currentTheme = AppTheme.Light;
+
+			// Đổi Theme
+			Application.Current.UserAppTheme = (currentTheme == AppTheme.Light)
+				? AppTheme.Dark
+				: AppTheme.Light;
+
+			UpdateThemeIcon();
+		}
+
+		private void UpdateThemeIcon()
+		{
+			if (Application.Current.UserAppTheme == AppTheme.Dark)
+			{
+				ThemeIcon.Text = "☀️";
+			}
+			else
+			{
+				ThemeIcon.Text = "🌙";
+			}
+		}
+	}
+}
