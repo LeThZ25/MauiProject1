bc0e4124581786437a264ddebe1b0721cb016bd8
nguyentrananhhoang13122005 <hoangvb11xs@gmail.com>
2026-01-05 09:20:48 +0700

    Delete MauiProject1 directory

--- MauiProject1/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiProject1")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiProject1.Pages")]
